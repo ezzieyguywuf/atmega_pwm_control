0afd95934db7ce4a96316a9596632c04bf37d6bb
Wolfgang E Sanyer <ezzieyguywuf@gmail.com>
2011-11-20 11:16:59 -0500

    Back to using 16-bit timer.
    I didn't have enough room in 8-bits to count out the length of time that
    I wanted. I may go back to the 8-bit timer if I end up using PWM

--- timer.c
+++ timer.c
@@ -25,18 +25,39 @@
 static uint8_t aref = (1<<REFS0); // default to AREF = Vcc
 
 int main(void){
-    CPU_PRESCALE(CPU_2MHz); // Note that I've used the 1MHz clock speed in order
-                            // to agree with the expamples in the pdf
+    // NOTE: look at ../clock_vals/clocks.py for a (configurable) table of the
+    // number of counts needed to reach a certain period at a given clock speed
+    // and prescaler value. For example, for a 1 second period:
+    //                  
+    //                            Pre_Scaler value
+    //  sys_clock|       1          8         64         256       1024    
+    //     MHz   |
+    //   --------| -------------------------------------------------------
+    //   0.062e6 |      62000       7750        968*       242*        60*
+    //   0.125e6 |     125000      15625       1953*       488*       122*
+    //   0.250e6 |     250000      31250       3906*       976*       244*
+    //   0.500e6 |     500000      62500       7812*      1953*       488*
+    //   1.000e6 |    1000000     125000      15625       3906*       976*
+    //   2.000e6 |    2000000     250000      31250       7812*      1953*
+    //   4.000e6 |    4000000     500000      62500      15625       3906*
+    //   8.000e6 |    8000000    1000000     125000      31250       7812*
+    //  16.000e6 |   16000000    2000000     250000      62500      15625
+    //
+    //  An asterix represents an non-whole number of counts. Note that for a
+    //  16-bit timer, the max value that can be counted is 0xFFFF, or 65535
+
+    CPU_PRESCALE(CPU_8MHz); 
 #if defined(DEBUG)
     usb_init();
 #endif
     DDRB |= (1 << 5); // Set Port B, pin 5 to output, as this correspondse to OC1A
 
-    // TODO set 8-bit timer to CTC  mode 
-    // TODO set count of timer 4 channel A to the equivalent of 1Hz
+    TCCR1A = (1 << COM0A0); // Set timer 1 chanel A to CTC mode
+    OCR1A = 0x3d09; // Set number of counts to the equivalent of 2 Hz see table
+                    // above. This is for a pre-scale of 256
 
 
-    // TODO start timer 4 at a prescale value of (64?)
+    // TODO start timer 1 at a prescale value of 256
 
     int last=-1, curr=0,i;
     curr = analogRead(PINF1);

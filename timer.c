de1718c1fd5f812a250ed2172d37eb6c7a9d817d
Wolfgang E Sanyer <ezzieyguywuf@gmail.com>
2011-11-20 15:26:18 -0500

    Timer 1 configured appropriately.
    I got timer 1 set up to output at 1 cycle / 2 seconds. I'm running into
    problems getting timer 0 to read this as input. Either that or I don't have
    the timer 0 output set up appropriately.

--- timer.c
+++ timer.c
@@ -5,11 +5,24 @@ static uint8_t aref = (1<<REFS0); // default to AREF = Vcc
 int main(void){
     CLKPR = 0x80; // Set clock to 'you can change me' mode
     CLKPR = 0x05; // Set clock to 0.5MHz
-    // TODO set up timer1, with a pre-scale of 8
-    // TODO set up timer1, channel A in CTC mode with an OCR1A=62,500, which
-    // equals 1 second
-    // TODO set up timer0 to external source, rising edge (input from timer1)
-    // TODO set up timer0 to output PWM
+
+    DDRB |= (1 << 5); // set up OC1A as output
+    DDRD &= ~(1 << 7); // set up T0 as input
+    DDRB |= (1 << 7);  // set up OC0A as output
+
+    TCCR1B |= (1 << CS01); // set up timer1, with a pre-scale of 8
+    TCCR1A |= (1 << COM1A0); // set up timer1, channel A in CTC toggle mode 
+    OCR1A = 62500; // set up OCR1A=62,500, which equals 1 second
+
+    // set up timer0 to external source, rising edge (input from timer1)
+    TCCR0B |= (1 << CS02 | 1 << CS01 | 1 << CS00);
+    TCCR0A |= (1 << WGM01 | 1 << WGM00); // set up timer0 to output PWM
+    TCCR0A |= (1 << WGM01); // set up timer0 for CTC
+    TCCR0A |= (1 << COM0A0 | 1 << COM0A1); // toggle OC0A on compare match
+
+    OCR0A = ~(1<<4) | (1 << 4);// initialize timer B with a 50% duty cycle
+    /*OCR0A = 0x08 ;// initialize timer B with a 100% duty cycle*/
+    OCR0A = 0x02; // Every two ticks
     while(1){
         // TODO monitor potentiometer
         // TODO change duty cycle based on pot location
@@ -18,3 +31,4 @@ int main(void){
     }
     return 0;
 }
+
